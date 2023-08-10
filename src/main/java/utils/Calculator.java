a3e195ad47c72f2eecd5df6687747f035271ba42
kandratsenka-solvd <akandratsenka.laba@solvd.com>
2023-08-10 21:53:21 +0300

    add: calculator multiply

--- src/main/java/utils/Calculator.java
+++ src/main/java/utils/Calculator.java
@@ -5,4 +5,8 @@ public class Calculator {
     public static Integer calculate(int a, int b) {
         return a + b;
     }
+
+    public static Integer calculateMultiply(int a, int b) {
+        return a * b;
+    }
 }
